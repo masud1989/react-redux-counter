fcc3820d007e681e0fa87fa11e92eb623d7827e0
masud1989 <mranasrj@gmail.com>
2022-08-23 15:25:08 +0600

    mapStateToProps-mapDispatchToProps

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,7 +8,7 @@ const Counter = ({ count, increment, decrement }) => {
             <div
                 className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
             >
-                <div className="text-2xl font-semibold">{count}</div>
+                <div className="text-3xl font-semibold">{count}</div>
                     <div className="flex space-x-3">
                         <button
                             className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
