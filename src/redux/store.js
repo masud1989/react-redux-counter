d6b66c015a6d40d637f24dcc111bed25f7dbc649
masud1989 <mranasrj@gmail.com>
2022-08-25 13:38:48 +0600

    Middleware concept

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+// create myLogger Middleware (Curried Function)
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upcommingState = [action].reduce(rootReducer, store.getState()) 
+    console.log(`Upcomming State: ${JSON.stringify(upcommingState)}`);
+    //Pass Action
+    return next(action)
+}
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
